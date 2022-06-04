c3f1e3a4701cc4101ee231cbb52411bfa00c72e1
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-06-04 09:42:04 +0530

    --- Auto-Git Commit ---

--- roboshop-basic-setup/ec2/ec2.tf
+++ roboshop-basic-setup/ec2/ec2.tf
@@ -15,7 +15,8 @@ resource "aws_spot_instance_request" "cheap_worker" {
       password = "DevOps321"
     }
     inline = [
-      "ansible-pul -U https://github.com/Varalakshmi13/ansible.git roboshop.yml -e HOST=localhost -e role_name=${var.COMPONENT} -e ENV=dev -e APP_VERSION=${var.APPVERSION}"
+      "ansible-pul -U https://github.com/Varalakshmi13/ansible.git roboshop.yml -e HOST=localhost -e role_name=${var.COMPONENT} -e ENV=dev -e 
+      APP_VERSION=${var.APPVERSION}"
     ]
 
   }
