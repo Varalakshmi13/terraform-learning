c0ce48795d2d1da8a352a5c83eecc87f33b65434
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-06-04 09:50:16 +0530

    --- Auto-Git Commit ---

--- roboshop-basic-setup/ec2/ec2.tf
+++ roboshop-basic-setup/ec2/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_spot_instance_request" "cheap_worker" {
-  ami           = data.aws_ami.ami.image_id
-  instance_type = "t3.micro"
-  wait_for_fulfillment = true
-  vps_security_group_ids = [aws_security_group.allow_all.id]
+  ami                     = data.aws_ami.ami.image_id
+  instance_type           = "t3.micro"
+  wait_for_fulfillment    = true
+  vps_security_group_ids  = [aws_security_group.allow_all.id]
 
   tags = {
     Name = var.COMPONENT
