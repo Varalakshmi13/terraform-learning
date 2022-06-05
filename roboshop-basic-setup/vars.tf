322dbb63e4bff881d366ee49dbb53e0693ca3d87
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-06-05 14:06:44 +0530

    --- Auto-Git Commit ---

--- roboshop-basic-setup/vars.tf
+++ roboshop-basic-setup/vars.tf
@@ -1,7 +1,13 @@
 variable "COMPONENTS" {
     default = [
         "mongodb",
-        "catalogue"
+        "catalogue",
+        "redis",
+        "user",
+        "cart",
+        "mysql",
+        "shipping",
+        "payment"
     ]
 }
 
@@ -10,5 +16,18 @@ variable "APP_VERSION" {
     default = [
         "null",
         "1.0.0"
+    ]
+}
+
+VA==variable "ALL_COMPONENTS" {
+    default = [
+        {
+            COMPONENT = "mongodb",
+            APP_VERSION = "1.0.0"
+        },
+        {
+            COMPONENT = "catalogue",
+            APP_VERSION = "1.0.0"
+        }
     ]
 }
